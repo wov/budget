9476df2f9bc506095344bdd39e28dc2e2a94e993
wov <wovfeng@gmail.com>
2021-07-11 23:36:53 +0800

    ready to show different period content

--- budget/views/Home.swift
+++ budget/views/Home.swift
@@ -10,94 +10,37 @@ import CoreData
 
 struct Home: View {
     @Environment(\.managedObjectContext) private var viewContext
+    var periodid:UUID
     
-    //TODO:管他呢，先把数据全部载入了吧....
-    //这里的init实在是搞不懂了...
-    @FetchRequest(
-        entity: Account.entity(),
-        sortDescriptors: [
-            NSSortDescriptor(keyPath: \Account.name, ascending: false)
-        ],
-        animation: .default
-    ) var accounts: FetchedResults<Account>
-    
-    @FetchRequest(
-        entity: CreatedIE.entity(),
-        sortDescriptors: [],
-        animation: .default
-    ) var ies: FetchedResults<CreatedIE>
-    
-    @FetchRequest(
-        entity: Period.entity(),
-        sortDescriptors: [
-        ],
-        animation: .default
-    ) var periods: FetchedResults<Period>
-    
-    @FetchRequest(
-        entity: System.entity(),
-        sortDescriptors: [],
-        animation: .default
-    ) var systems: FetchedResults<System>
-    
-    
+    @FetchRequest var accounts : FetchedResults<Account>
+    @FetchRequest var ies : FetchedResults<CreatedIE>
     @State private var showAddAccount: Bool = false
-    @State private var showAddPeriod: Bool = false
-    
-    //Temp code for delete period test.
-    private func deletePeriods(){
-        let period = self.periods[0]
-        viewContext.delete(period)
-        do {
-            try viewContext.save()
-        } catch {
-            
-        }
-    }
-    
-    var body: some View {
-        if(periods.isEmpty){
-            NavigationView{
-                VStack{
-                    Text("当前还未创建账期")
-                    Button(action: {
-                        self.showAddPeriod.toggle()
-                    }){
-                        Text("创建账期")
-                    }
-                }
-            }
-            .sheet(isPresented: $showAddPeriod, content: {
-                addPeriod(showAddPeriod:self.$showAddPeriod)
-            })
-        }else{
-            NavigationView {
-                List{
-                    ForEach(accounts) { account in
-                        AccountsRow(account:account,ies:ies)
-                    }
-                }
-                .listStyle(GroupedListStyle())
-                .navigationTitle("2021-06")
-                .toolbar(content: {
-                    Button(action: {
-                        self.showAddAccount.toggle()
-                    }){
-                        Text("管理账户")
-                    }
-                })
-            }
-            .sheet(isPresented: $showAddAccount, content: {
-                addAccount(showAddAccount:self.$showAddAccount)
-            })
-            
-            
-        }
-    }
-}
 
-struct Home_Previews: PreviewProvider {
-    static var previews: some View {
-        Home()
+    init(_ periodid:UUID ) {
+        self.periodid = periodid
+        self._accounts = FetchRequest(entity: Account.entity(), sortDescriptors: [])
+        self._ies = FetchRequest(entity: CreatedIE.entity(), sortDescriptors: [] ,predicate: NSPredicate(format: "period == %@", periodid as CVarArg))
+    }
+        
+    var body: some View {
+        NavigationView {
+            List{
+                ForEach(accounts) { account in
+                    AccountsRow(account:account,ies:ies)
+                }
+            }
+            .listStyle(GroupedListStyle())
+            .navigationTitle("2021-06")
+            .toolbar(content: {
+                Button(action: {
+                    self.showAddAccount.toggle()
+                }){
+                    Text("管理账户")
+                }
+            })
+        }
+        .sheet(isPresented: $showAddAccount, content: {
+            addAccount(showAddAccount:self.$showAddAccount)
+        })
     }
 }
