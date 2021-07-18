03558230448c33faf99f785c05427213f0bb5a26
wov <wovfeng@gmail.com>
2021-07-18 19:08:56 +0800

    now can delete period and delete ie that belong it

--- budget/views/PeriodList.swift
+++ budget/views/PeriodList.swift
@@ -16,14 +16,32 @@ struct PeriodList: View {
         animation: .default
     ) var periods: FetchedResults<Period>
     
+    @FetchRequest(
+        entity: CreatedIE.entity(),
+        sortDescriptors: [],
+        animation: .default
+    ) var createdies: FetchedResults<CreatedIE>
     
     
-    
-    
-    
-    
-    
-    
+    private func deletePeriods(offsets: IndexSet){
+        for offset in offsets {
+            let period = periods[offset]
+            
+            for ie in createdies{
+                if(ie.period! == period.id!){
+                    viewContext.delete(ie)
+                }
+            }
+            
+            viewContext.delete(period)
+        }
+        
+        do {
+            try viewContext.save()
+        } catch {
+            
+        }
+    }
     
     
     var body: some View {
@@ -35,8 +53,7 @@ struct PeriodList: View {
                             Text( String("\(period.year!)-\(period.month!)")  )
                         }
                     }
-
-                }
+                }.onDelete(perform: deletePeriods)
             }
             .listStyle(GroupedListStyle())
             .navigationTitle("选择月份")
